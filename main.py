c0052a474d3e98d51d90920837659cbd3eb0c96a
faroemiliano <emilianofaro@MacBook-Pro-de-Emiliano.local>
2026-02-12 20:39:45 -0300

    correccion cors

--- main.py
+++ main.py
@@ -22,9 +22,10 @@ Base.metadata.create_all(bind=engine)
 # =====================
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-       
-        "https://front-barberia1991.vercel.app"
+   allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://front-barberia1991.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
