f8cdb697210d63bf6b831c25cea6a843be96acd8
faroemiliano <emilianofaro@MacBook-Pro-de-Emiliano.local>
2026-02-12 22:52:51 -0300

    borrar debug y crear datos para db1

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ print(os.getenv("ADMIN_EMAIL"))
 from database import engine
 from models import Base
 
-from routers import auth, calendario, admin, auth_google, admin_servicios, debug
+from routers import auth, calendario, admin, auth_google, admin_servicios
 
 
 app = FastAPI(title="Barbería API")
@@ -40,4 +40,3 @@ app.include_router(calendario.router, prefix="", tags=["Calendario"])
 app.include_router(admin.router, prefix="/admin", tags=["Admin"])
 app.include_router(auth_google.router)
 app.include_router(admin_servicios.router)
-app.include_router(debug.router)
