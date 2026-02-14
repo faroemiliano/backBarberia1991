a2936920130664323226b9aabc8684937a40cd56
faroemiliano <emilianofaro@MacBook-Pro-de-Emiliano.local>
2026-02-14 20:06:09 -0300

    custom_openapi

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 import os
@@ -11,6 +12,31 @@ from routers import auth, calendario, admin, auth_google, admin_servicios
 
 
 app = FastAPI(title="Barbería API")
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+
+    openapi_schema = get_openapi(
+        title="Barberia API",
+        version="1.0",
+        description="API de turnos",
+        routes=app.routes,
+    )
+
+    openapi_schema["components"]["securitySchemes"] = {
+        "BearerAuth": {
+            "type": "http",
+            "scheme": "bearer",
+            "bearerFormat": "JWT"
+        }
+    }
+
+    openapi_schema["security"] = [{"BearerAuth": []}]
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+app.openapi = custom_openapi
 
 # =====================
 # BASE DE DATOS 
